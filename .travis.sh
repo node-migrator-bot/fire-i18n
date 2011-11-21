89ccf05715f9a6fe7df8bf9ed9f9a61f0633bc06
Johan Hernandez <johan@firebase.co>
2011-11-21 10:29:06 -0500

    Removed runner from travis script

--- .travis.sh
+++ .travis.sh
@@ -2,7 +2,3 @@
 
 curl -# https://raw.github.com/firejs/travis-ci-scripts/master/main.sh | bash
 
-travis_build_name="fire-i18n"
-. .travis_temp/runner.sh
-. .travis_temp/$travis_build_name.sh
-travis_runner
